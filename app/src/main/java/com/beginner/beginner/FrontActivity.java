f31d2902e24f950270296ee8c77a5ee635cf3dbe
mgeraldhia <geraldhia@gmail.com>
2017-05-07 10:11:45 +0700

    V1 menambahkan 2 activity dengan menerapkan intent

---
+++ app/src/main/java/com/beginner/beginner/FrontActivity.java
@@ -0,0 +1,30 @@
+package com.beginner.beginner;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FrontActivity extends AppCompatActivity {
+
+    Button tombol;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_front);
+
+        tombol = (Button) findViewById(R.id.Masuk);
+        tombol.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i;
+                i = new Intent(FrontActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        }
+        );
+
+    }
+}
